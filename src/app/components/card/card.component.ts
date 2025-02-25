ad462345f3bacd9164361e4b7244c692109e1a45
drlcoco <drlcoco@hotmail.com>
2025-02-26 00:51:04 +0100

    Deploy 25

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -39,11 +39,11 @@ export class CardComponent implements OnInit, AfterViewInit {
         this.currentId = this.project6;
         break;
     }
-    this.cdr.detectChanges(); // Forzar detección de cambios
+    this.cdr.detectChanges();
   }
 
   ngAfterViewInit() {
-    this.cdr.detectChanges(); // Fuerza a Angular a ejecutar la detección de cambios nuevamente
+    this.cdr.detectChanges();
   }
 
   setImage(src: string): void {
